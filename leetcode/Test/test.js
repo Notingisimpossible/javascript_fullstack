88f2efcada7f3322f3a254d603dd19fb921a8d92
Notingisimpossible <2421665649@qq.com>
2019-10-20 20:24:01 +0800

    test

--- leetcode/Test/test.js
+++ leetcode/Test/test.js
@@ -4,53 +4,95 @@
  */
 var candy = function(ratings) {
 	let number=0
-	let list = new Array()
+	let list1 = new Array()
+	let list2 = new Array()
+	let list3 = new Array()
 	for(let i=0;i<ratings.length;i++){
-		list[i]=1
+		list1[i]=1
+		list2[i]=1
 	}
 	for (let i = 1; i < ratings.length-1; i++) {
 		if(ratings[i]>ratings[i-1]&&ratings[i]<ratings[i+1]){
-			list[i]=list[i-1]+1
+			list1[i]=list1[i-1]+1
 		}
 		else if(ratings[i]<ratings[i-1]&&ratings[i]>ratings[i+1]){
-			list[i]=list[i+1]+1
+			list1[i]=list1[i+1]+1
 		}
 		else if(ratings[i]<ratings[i-1]&&ratings[i]<ratings[i+1]){
-			list[i]=Math.min(list[i-1],list[i+1])-1
+			list1[i]=1
 		}
 		else if(ratings[i]==ratings[i-1]&&ratings[i]<ratings[i+1]){
-			if(list[i+1]!=1){
-				list[i]=[i+1]-1
-			}
+
 		}
 		else if(ratings[i]==ratings[i-1]&&ratings[i]>ratings[i+1]){
-			list[i]=list[i+1]+1
+			list1[i]=list1[i+1]+1
 		}
 		else if(ratings[i]<ratings[i-1]&&ratings[i]==ratings[i+1]){
-			if(list[i-1]!=1){
-				list[i]=list[i-1]-1
-			}
+
 		}
 		else if(ratings[i]>ratings[i-1]&&ratings[i]==ratings[i+1]){
-			list[i]=list[i-1]+1
+			list1[i]=list1[i-1]+1
 		}
 		else if(ratings[i]==ratings[i-1]&&ratings[i]==ratings[i+1]){
-			list[i]=1
+
 		}
 		else{
-			list[i]=Math.max(list[i-1],list[i+1])+1
+			list1[i]=Math.max(list1[i-1],list1[i+1])+1
 		}
 	}
 	if(ratings[0]>ratings[1]){
-		list[0]=list[1]+1
+		list1[0]=list1[1]+1
 	}
-	if(ratings[ratings.length-1]>ratings.length-2){
-		list[ratings.length-1]=list[ratings.length-2]
+	if(ratings[ratings.length-1]>ratings[ratings.length-2]){
+		list1[ratings.length-1]=list1[ratings.length-2]+1
 	}
-	for(let i=0;i<list.length;i++){
-		number+=list[i]
+	for (let i = ratings.length-2; i >0 ; i--) {
+		if(ratings[i]>ratings[i-1]&&ratings[i]<ratings[i+1]){
+			list2[i]=list2[i-1]+1
+		}
+		else if(ratings[i]<ratings[i-1]&&ratings[i]>ratings[i+1]){
+			list2[i]=list2[i+1]+1
+		}
+		else if(ratings[i]<ratings[i-1]&&ratings[i]<ratings[i+1]){
+
+		}
+		else if(ratings[i]==ratings[i-1]&&ratings[i]<ratings[i+1]){
+
+		}
+		else if(ratings[i]==ratings[i-1]&&ratings[i]>ratings[i+1]){
+			list2[i]=list2[i+1]+1
+		}
+		else if(ratings[i]<ratings[i-1]&&ratings[i]==ratings[i+1]){
+
+		}
+		else if(ratings[i]>ratings[i-1]&&ratings[i]==ratings[i+1]){
+			list2[i]=list2[i-1]+1
+		}
+		else if(ratings[i]==ratings[i-1]&&ratings[i]==ratings[i+1]){
+
+		}
+		else{
+			list2[i]=Math.max(list2[i-1],list2[i+1])+1
+		}
 	}
-	console.log(list)
+	if(ratings[0]>ratings[1]){
+		list2[0]=list2[1]+1
+	}
+	if(ratings[ratings.length-1]>ratings[ratings.length-2]){
+		list2[ratings.length-1]=list2[ratings.length-2]+1
+	}
+	for(let i=0;i<list1.length;i++){
+		if(list1[i] != list2[i]){
+			list3[i]=Math.max(list1[i],list2[i])
+		}
+		else{
+			list3[i]=list1[i]
+		}
+		number+=list3[i]
+	}
+	console.log(list1)
+	console.log(list2)
+	console.log(list3)
 	return number
 };
 
