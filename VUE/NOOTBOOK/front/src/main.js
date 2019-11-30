548d9eb85c64225db867ec07dee93d2b6b4a6839
Notingisimpossible <2421665649@qq.com>
2019-11-30 14:01:31 +0800

    vue前端+node-koa后端

--- VUE/NOOTBOOK/front/src/main.js
+++ VUE/NOOTBOOK/front/src/main.js
@@ -3,13 +3,27 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { Swipe, SidebarItem, Icon, Field, ActionSheet, Toast, Panel, Uploader, SwipeItem } from 'vant'
+import 'vant/lib/index.css'
+import axios from 'axios'
+import 'amfe-flexible' //字体
+
+
+Vue.use(Swipe).use(SwipeItem).use(Icon).use(Field).use(ActionSheet).use(Toast).use(Panel).use(Uploader).use(axios)
+
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, form, next) => {
+  document.title = to.meta.title
+  console.log(to)
+  next()
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-})
+})     
