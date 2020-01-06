2f3ba94520a7294f8629d5052dfaea311ff78c59
Notingisimpossible <2421665649@qq.com>
2020-01-06 17:12:52 +0800

    vue3.0 BlogDemo

---
+++ VUE3.0/blog-demo/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>cliverSnail's blog</h1>
+    <BlogPage></BlogPage>
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+  import BlogPage from './components/BlogPage.vue';
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component({
+    components: {
+      BlogPage,
+    },
+  })
+
+  export default class App extends Vue {
+
+  }
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
